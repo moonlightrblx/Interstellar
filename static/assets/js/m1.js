3e86436200ac0ec3fd39669dbfbc8a9efa6e3e6f
ellii <patrick@savells.net>
2025-11-12 09:55:23 +0000

    newest thingy

--- static/assets/js/m1.js
+++ static/assets/js/m1.js
@@ -35,9 +35,9 @@ document.addEventListener("DOMContentLoaded", () => {
         <a class="icon" href="/./"><img alt="nav" id="INImg" src="${LogoUrl}"/></a>
       </div>
       <div class="f-nav-right">
-        <a class="navbar-link" href="/./a"><i class="fa-solid fa-gamepad navbar-icon"></i><an>&#71;&#97;</an><an>&#109;&#101;&#115;</an></a>
-        <a class="navbar-link" href="/./b"><i class="fa-solid fa-phone navbar-icon"></i><an>&#65;&#112;</an><an>&#112;&#115;</an></a>
-        ${qp ? "" : '<a class="navbar-link" href="/./d"><i class="fa-solid fa-laptop navbar-icon"></i><an>&#84;&#97;</an><an>&#98;&#115;</an></a>'}
+        <a class="navbar-link" href="/./a"><i class="fa-solid fa-heart navbar-icon"></i><an>&#71;&#97;</an><an>&#109;&#101;&#115;</an></a>
+        <a class="navbar-link" href="/./b"><i class="fa-solid fa-play navbar-icon"></i><an>&#65;&#112;</an><an>&#112;&#115;</an></a>
+        ${qp ? "" : '<a class="navbar-link" href="/./d"><i class="fa-brands fa-firefox navbar-icon"></i><an>&#84;&#97;</an><an>&#98;&#115;</an></a>'}
       </div>`;
     nav.innerHTML = html; //    <a class="navbar-link" href="/./c"><i class="fa-solid fa-gear navbar-icon settings-icon"></i><an>&#83;&#101;&#116;</an><an>&#116;&#105;&#110;&#103;</an></a>
   }
